b53390220320e4927b7bfd8bede9966685f09be8
Zainul <zainulaziz29@gmail.com>
2024-03-14 00:38:53 +0530

    Connection changes

--- signupbackend/server.js
+++ signupbackend/server.js
@@ -3,9 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const routesUrls = require("./routes/routes");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
+dotenv.config({ path: "./config.env" });
+const DB = process.env.DB_CONNECT;
 mongoose
-  .connect("mongodb://localhost:27017/EventMaster", {
+  // .connect("mongodb://localhost:27017/EventMaster", {
+  .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -14,13 +18,13 @@ mongoose
     console.log(`connection successful`);
   })
   .catch((e) => {
-    console.log(`No Connection`);
+    console.log(`No Connection` + e);
   });
 
 app.use(express.json());
 app.use(cors());
 app.use("/app", routesUrls);
 
-app.listen(4000, () => {
-  console.log(`server is running at port no 4000`);
+app.listen(process.env.PORT, () => {
+  console.log(`server is running at port no: ${process.env.PORT}`);
 });
